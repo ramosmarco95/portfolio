a86e0149136e28263bec8bc0b9e064e9cb71de12
tva.marco <ramosmarco95@gmail.com>
2026-05-14 13:38:42 -0600

    bootstrap removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css' // Added Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Added Bootstrap JS
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
-  </StrictMode>
-)
+  </StrictMode>,
+);
